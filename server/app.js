1d48681c3b980d6d9f1213764773db4bbe4f04a8
Julien Andre <julienandre1199@gmail.com>
2016-12-15 11:59:19 +0100

    [Refactoring] downgraded to simplified version

--- server/app.js
+++ server/app.js
@@ -1,71 +1,74 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
-
-
-
-
 // Init globals variables for each module required
-var app = express()
-  , http = require('http')
+var http = require('http')
   , server = http.createServer(app)
   , io = require('socket.io').listen(server);
 
 // launch the http server on given port
 server.listen(8082);
 
+// usernames which are currently connected to the chat
+var usernames = {};
+var listOfPlayers = {};
 
+io.sockets.on('connection', function (socket) {
+  // when the public emits 'sendchat', this listens and executes
+  socket.on('sendchat', function (data) {
+    // we tell the public to execute 'updatechat' with 2 parameters
+    io.sockets.emit('updatechat', socket.username, data);
+  });
 
+  // when the public emits 'sendchat', this listens and executes
+  socket.on('sendpos', function (newPos) {
+    // we tell the public to execute 'updatepos' with 2 parameters
+    //console.log("recu sendPos");
+    socket.broadcast.emit('updatepos', socket.username, newPos);
+  });
 
+  // when the public emits 'adduser', this listens and executes
+  socket.on('adduser', function(username){
+    // we store the username in the socket session for this public
+    // the 'socket' variable is unique for each public connected,
+    // so we can use it as a sort of HTTP session
+    socket.username = username;
+    // add the public's username to the global list
+    // similar to usernames.michel = 'michel', usernames.toto = 'toto'
+    usernames[username] = username;
+    // echo to the current public that he is connecter
+    socket.emit('updatechat', 'SERVER', 'you have connected');
+    // echo to all public except current, that a new person has connected
+    socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+    // tell all clients to update the list of users on the GUI
+    io.sockets.emit('updateusers', usernames);
 
+    // Create a new player and store his position too... for that
+    // we have an object that is a "list of players" in that form
+    // listOfPlayer = {'michel':{'x':0, 'y':0, 'v':0},
+    // 							john:{'x':10, 'y':10, 'v':0}}
+    // for this example we have x, y and v for speed... ?
+    var player = {'x':0, 'y':0, 'v':0};
+    listOfPlayers[username] = player;
+    io.sockets.emit('updatePlayers',listOfPlayers);
+  });
 
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function(){
+    // remove the username from global usernames list
+    delete usernames[socket.username];
+    // update list of users in chat, public-side
+    io.sockets.emit('updateusers', usernames);
 
+    // Remove the player too
+    delete listOfPlayers[socket.username];
+    io.sockets.emit('updatePlayers',listOfPlayers);
 
-
-
-
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // echo globally that this public has left
+    socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+  });
 });
 
 module.exports = app;
