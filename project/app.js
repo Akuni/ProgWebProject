0b8951ca9caeef2b15b615a0963b82ef3f397138
Julien Andre <julienandre1199@gmail.com>
2017-01-05 11:01:22 +0100

    [Refactor] html back to jade in order to inject json into views [Adding] debug page (error print in incomplete) [Adding] session (error spotted, waiting for fix which is pending because i am building the debug page)

--- project/app.js
+++ project/app.js
@@ -1,37 +1,43 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var index = require('./routes/index');
 var play = require('./routes/play');
 var signup = require('./routes/sign_up');
 var signin = require('./routes/sign_in');
 var admin = require('./routes/admin');
+var session_test = require('./routes/session_test');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.set('view engine', 'jade');
+// Comment top line and uncomment the 2 bottom line to switch between html and jade
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use('/', index);
 app.use('/play', play);
 app.use('/sign_up', signup);
 app.use('/sign_in', signin);
 app.use('/admin', admin);
+app.use('/session', session_test);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -46,9 +52,17 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  console.log(err);
+  console.log(res);
+  console.log(res.locals.error);
+
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  console.log(err);
+  if(err.status != 404)
+    res.render('debug', { "err" : err.error });
+  else
+    res.render('error');
 });
 
 module.exports = app;
