36799148acae30a0a5707dae1b98f813bf45d040
antoinerollin <antoine.rollin26@free.fr>
2017-01-09 17:35:08 +0100

    ADD Team view

--- project/public/js/admin_enigma.js
+++ project/public/js/admin_enigma.js
@@ -141,24 +141,7 @@ var setStatusCorrectnessStyle = function(elem, isWrong){
 };
 
 var map, addedMarker;
-// function initMap() {
-//     getLocation();
-//     var mapOptions = {
-//         zoom : 17,
-//         center : new google.maps.LatLng(0, 0)
-//     };
-//     map = new google.maps.Map(document.getElementById('map'), mapOptions);
-//     google.maps.event.addListener(map, 'click', function (event) {
-//         placeMarker(event.latLng);
-//     });
-//     // listen for the window resize event & trigger Google Maps to update too
-//     // $(window).resize(function () {
-//     //     // (the 'map' here is the result of the created 'var map = ...' above)
-//     //     google.maps.event.trigger(map, "resize");
-//     // });
-// }
 
-//google.maps.event.addDomListener(window, 'load', initialize);
 function pan(x,y) {
     var panPoint = new google.maps.LatLng(x, y);
     map.setCenter(panPoint);
---
+++ project/public/js/team.js
@@ -0,0 +1,74 @@
+var conversation, data, datasend, users;
+
+var socket = io.connect();
+
+// on connection to server, ask for user's name with an anonymous callback
+socket.on('connect', function(){
+    socket.emit('adduser', "admin#"+socket.id.toString().slice(1,5));
+});
+
+// listener, whenever the server emits 'updatechat', this updates the chat body
+socket.on('updatechat', function (username, data) {
+    var chatMessage = "<b>" + username + ":</b> " + data + "<br>";
+    conversation.innerHTML += chatMessage;
+});
+
+// listener, whenever the server emits 'updateusers', this updates the username list
+socket.on('updateusers', function(listOfUsers) {
+    users.innerHTML = "";
+    for(var name in listOfUsers) {
+        var userLineOfHTML = '<div>' + name + '</div>';
+        users.innerHTML += userLineOfHTML;
+    }
+});
+
+socket.on('getteams', function (data) {
+    console.log(JSON.stringify(data));
+
+    var teams_table = document.querySelector("#teams_table");
+
+    var content = "";
+    data.forEach(function(elem){
+        content += '<tr>'
+            + '<td>' + elem.name + '</td>'
+            + '<td>' + elem.score + '</td>'
+            + '<td>' + elem.email + '</td>'
+            + '</tr>';
+    });
+
+    teams_table.innerHTML = content;
+});
+
+// on load of page
+window.addEventListener("load", function(){
+
+    socket.emit('getteams');
+
+    // get handles on various GUI components
+    conversation = document.querySelector("#conversation");
+    data = document.querySelector("#data");
+    datasend = document.querySelector("#datasend");
+    users = document.querySelector("#users");
+
+    // Listener for send button
+    datasend.addEventListener("click", function(evt) {
+        sendMessage();
+    });
+
+    // detect if enter key pressed in the input field
+    data.addEventListener("keypress", function(evt) {
+        // if pressed ENTER, then send
+        if(evt.keyCode == 13) {
+            this.blur();
+            sendMessage();
+        }
+    });
+
+    // sends the chat message to the server
+    function sendMessage() {
+        var message = data.value;
+        data.value = "";
+        // tell server to execute 'sendchat' and send along one parameter
+        socket.emit('sendchat', message);
+    }
+});
